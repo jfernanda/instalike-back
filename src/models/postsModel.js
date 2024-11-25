fd4a42a6ffe1e8a358477e3f588858dc3f81302b
J. Fernanda <santosl.jfer@gmail.com>
2024-11-25 22:49:47 +0000

    Armazenamento e upload de imagem .png

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,9 +1,23 @@
+import { ObjectId } from "mongodb";
 import conectarAoBanco from "../config/dbConfig.js";
 
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-export default async function getTodosPosts() {
+export async function getTodosPosts() {
     const db = conexao.db("imersao-instalike");
     const colecao = db.collection("posts");
     return colecao.find().toArray();
+}
+
+export async function criarPost(novoPost) {
+    const db = conexao.db("imersao-instalike");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost);
+}
+
+export async function atualizarPost(id, novoPost) {
+    const db = conexao.db("imersao-instalike");
+    const colecao = db.collection("posts");
+    const objID = ObjectId.createFromHexString(id);
+    return colecao.updateOne(novoPost);
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,9 +1,26 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsControllers.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsControllers.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({dest:"./uploads", storage});
 
 const routes = (app) => {
     app.use(express.json());
     app.get("/posts", listarPosts);
+    app.post("/posts", postarNovoPost);
+    app.post("/upload", upload.single("imagem"), uploadImagem);
+    app.put("/upload/:id", atualizarNovoPost);
 }
 
 export default routes;
+
+//principais verbos HTTP: post, get, put e delete
